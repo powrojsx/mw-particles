05e8a481dd4715adc348b693d2315b1f21d0fbfa
KPowroznik <kpowroznik.dev@gmail.com>
2018-01-31 17:41:59 +0100

    finish

--- particles.js
+++ particles.js
@@ -1,13 +1,14 @@
 class Particle {
-    constructor(canvas, ctx) {
+    constructor(canvas, ctx, size) {
         this.canvas = canvas;
         this.ctx = ctx;
+        this.size = size;
         this.x = Math.random() * this.canvas.width;
         this.y = Math.random() * this.canvas.height;
 
         this.velocity = {
-            x: 0.5,
-            y: 0.5
+            x: Math.random() * 2,
+            y: Math.random() * 2
         }
     }
 
@@ -26,7 +27,7 @@ class Particle {
     drawParticle() {
         this.ctx.beginPath();
         this.ctx.fillStyle = "#000000";
-        this.size = Math.random() * 8;
+        // this.size = size;
         this.ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, true);
         this.ctx.fill();
         this.ctx.strokeStyle = "#ffffff";
@@ -41,8 +42,6 @@ class Particles {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext('2d');
-        // this.ctx.fillStyle = "#353638";
-        // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.initNetwork();
         this.updateParticles();
 
@@ -50,11 +49,9 @@ class Particles {
 
     initNetwork() {
         this.particles = [];
-        for (let i = 0; i < 50; i++) {
-            this.particles.push(new Particle(this.canvas, this.ctx));
+        for (let i = 0; i < 70; i++) {
+            this.particles.push(new Particle(this.canvas, this.ctx, Math.random() * 6));
         }
-
-        requestAnimationFrame(this.updateParticles());
     }
 
     updateParticles() {
@@ -63,9 +60,23 @@ class Particles {
         for(let i = 0; i < this.particles.length; i++) {
             this.particles[i].updateParticle();
             this.particles[i].drawParticle();
+
+            for(let g = this.particles.length - 1; g > i; g--) {
+                let distance = Math.hypot(this.particles[i].x - this.particles[g].x, this.particles[i].y - this.particles[g].y);
+                if (distance > 110) {
+                    continue;
+                }
+
+                this.ctx.beginPath();
+                this.ctx.strokeStyle = "#ffffff";
+                this.ctx.lineWidth = 1;
+                this.ctx.moveTo(this.particles[i].x, this.particles[i].y);
+                this.ctx.lineTo(this.particles[g].x, this.particles[g].y);
+                this.ctx.stroke();
+            }
         }
 
-        
+        requestAnimationFrame(this.updateParticles.bind(this));
     }
 }
 
